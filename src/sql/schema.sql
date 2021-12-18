dccbef330f0cb12cd98dff534e99c66a1dcb0054
Jack Cunningham <jackcunningham@u.boisestate.edu>
2021-12-18 14:48:22 -0700

    schema ready!

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -4,7 +4,8 @@
 
   NOTE: This file must be able to run repetitively without any errors
 */
-DATABASE IF NOT EXISTS cs_hu_310_final_project; 
+show tables;
+CREATE DATABASE IF NOT EXISTS cs_hu_310_final_project; 
 USE cs_hu_310_final_project; 
 DROP TABLE IF EXISTS class_registrations; 
 DROP TABLE IF EXISTS grades; 
@@ -12,7 +13,7 @@ DROP TABLE IF EXISTS class_sections;
 DROP TABLE IF EXISTS instructors; 
 DROP TABLE IF EXISTS academic_titles; 
 DROP TABLE IF EXISTS students; 
-DROP TABLE IF EXISTS classes 
+DROP TABLE IF EXISTS classes; 
 DROP FUNCTION IF EXISTS convert_to_grade_point; 
  
 CREATE TABLE IF NOT EXISTS classes( 
@@ -23,6 +24,7 @@ CREATE TABLE IF NOT EXISTS classes(
     maximum_students INT DEFAULT 10, 
     PRIMARY KEY(class_id) 
 ); 
+DESCRIBE classes;
  
 CREATE TABLE IF NOT EXISTS students( 
     student_id INT AUTO_INCREMENT, 
@@ -31,16 +33,87 @@ CREATE TABLE IF NOT EXISTS students(
     birthdate DATE, 
     PRIMARY KEY (student_id) 
 ); 
+DESCRIBE students;
  
 CREATE TABLE IF NOT EXISTS academic_titles(
     academic_title_id INT AUTO_INCREMENT NOT NULL, 
     title VARCHAR(255) NOT NULL, 
     PRIMARY KEY (academic_title_id)
 );
+describe academic_titles;
 
 CREATE TABLE IF NOT EXISTS instructors(
-    instructor_id INT AUTO_INCREMENT NOT NULL, 
-    first_name VARCHAR(80) NOT NULL, 
-    last_name VARCHAR(80) NOT NULL,
-    -- make academic_title_id foreighn key
-)
+	instructor_id INT AUTO_INCREMENT NOT NULL,
+	first_name VARCHAR(80) NOT NULL, 
+	last_name VARCHAR(80) NOT NULL, 
+    academic_title_id INT,
+    FOREIGN KEY (academic_title_id) REFERENCES academic_titles(academic_title_id),
+    PRIMARY KEY (instructor_id)
+);
+describe instructors;
+
+CREATE TABLE IF NOT EXISTS terms(
+    term_id INT AUTO_INCREMENT NOT NULL, 
+    name VARCHAR(80) NOT NULL, 
+    PRIMARY KEY (term_id)
+);
+describe terms;
+
+CREATE TABLE IF NOT EXISTS class_sections(
+	class_section_id INT AUTO_INCREMENT NOT NULL, 
+    class_id INT NOT NULL, 
+    FOREIGN KEY (class_id) references classes(class_id),
+    instructor_id INT NOT NULL, 
+    FOREIGN KEY (instructor_id) REFERENCES instructors(instructor_id),
+    term_id INT NOT NULL,
+    FOREIGN KEY (term_id) REFERENCES terms(term_id),
+    PRIMARY KEY (class_section_id)
+);
+describe class_sections;
+
+CREATE TABLE IF NOT EXISTS grades(
+    grade_id INT AUTO_INCREMENT NOT NULL, 
+    letter_grade CHAR(2) NOT NULL, 
+    PRIMARY KEY (grade_id)
+);
+describe grades;
+
+CREATE TABLE IF NOT EXISTS class_registrations(
+    class_registration_id INT AUTO_INCREMENT NOT NULL, 
+    class_section_id INT NOT NULL,
+    FOREIGN KEY (class_section_id) REFERENCES class_sections(class_section_id), 
+    student_id INT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    grade_id INT,
+    FOREIGN KEY (grade_id) REFERENCES grades(grade_id),
+    signup_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (class_registration_id)
+);
+describe class_registrations;
+
+ALTER TABLE class_registrations 
+ADD CONSTRAINT UC_student UNIQUE (class_section_id, student_id);
+
+
+DELIMITER $$
+CREATE FUNCTION convert_to_grade_point(letter_grade char(2))
+	RETURNS INT 
+    DETERMINISTIC
+BEGIN
+	DECLARE grade_point int;
+    
+    IF letter_grade like 'A' THEN
+		SET grade_point = 4;
+	ELSEIF letter_grade like 'B' THEN
+		SET grade_point = 3;
+	ELSEIF letter_grade like 'C' THEN 
+		set grade_point = 2;
+	ELSEIF letter_grade like 'D' THEN 
+		SET grade_point = 1;
+	ELSEIF letter_grade like 'F' THEN 
+		SET grade_point = 0;
+	ELSE 
+		SET grade_point = NULL;
+	END IF;
+    RETURN grade_point;
+END $$
